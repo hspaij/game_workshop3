036c4b82e3ef0df01627e8df621cb542b213e3c9
Pieter <pieter@sensingit.nl>
2020-10-02 13:41:09 +0100

    add movement to box and npc

---
+++ workshop3/Assets/Scripts/StalkScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StalkScript : MonoBehaviour
+{
+ public    Transform Player;
+ public int MoveSpeed = 2;
+ public int MaxDist = 10;
+ public int MinDist = 5;
+ 
+ 
+ 
+ 
+ void Start () 
+ {
+ 
+ }
+ 
+ void Update () 
+ {
+     transform.LookAt(Player);
+     
+     if(Vector3.Distance(transform.position,Player.position) >= MinDist){
+     
+          transform.position += transform.forward*MoveSpeed*Time.deltaTime;
+ 
+           
+           
+          if(Vector3.Distance(transform.position,Player.position) <= MaxDist)
+              {
+                 //Here Call any function U want Like Shoot at here or something
+    } 
+    
+    }
+ }
+}
